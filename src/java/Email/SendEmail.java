3e19068f16e26c2223472247d780b2d8dd93ca53
ReedS <ReedStock1992@gmail.com>
2018-04-22 23:55:45 -0500

    - Created skeleton of site. - Finish login   - Update firebase DB - Finish create account   - Update firebase DB - Fix navbar sessions

---
+++ src/java/Email/SendEmail.java
@@ -0,0 +1,61 @@
+/*
+ * Author: Reed Stock
+ * Date: 4/11/2018
+ * Description: Servlet used to send email with provided messages to provided email
+ * addresses.
+*/
+
+// Name of package
+package Email;
+
+// Import lines - Libraries
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+// Send email class
+public class SendEmail {
+    public static void sendEmail(String host, String port,
+            final String userName, final String password, String toAddress,
+            String subject, String message) throws AddressException,
+            MessagingException {
+ 
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+ 
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        msg.setText(message);
+ 
+        // sends the e-mail
+        Transport.send(msg);
+    }
+}

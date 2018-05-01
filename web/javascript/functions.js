0bf461e7a4e30b64ff4353e35c0116c65797bf3a
ReedS <ReedStock1992@gmail.com>
2018-05-01 09:44:34 -0500

    - Working skeleton

--- web/javascript/functions.js
+++ web/javascript/functions.js
@@ -13,7 +13,7 @@ var dayCounts =
   "Thursday" : 0,
   "Friday" : 0,
   "Saturday" : 0
-}
+};
 
 userRef.child("0/history").once("value",function(snap)
 {
@@ -205,4 +205,60 @@ function login(){
     else{
         window.location.replace("http://localhost:8080/Project-IoT/forgotpassword.jsp");
     }*/
+}
+
+
+
+
+
+/*NOTE: all of these only work if running from an HTTP server*/
+//test make user in firebase
+function makeUserAuth()
+{
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pass").value;
+  console.log("make auth: " + email + " " + password);
+  firebase.auth().createUserWithEmailAndPassword(email,password);
+}
+//test sign in
+function signIn()
+{
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pass").value;
+  console.log("attempting sign in: " + email + " ...");
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error)
+  {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  }).then(function()  //this is because the auth() functions are asyncronous. so this will wait
+  {
+    if( firebase.auth().currentUser === null ) {
+      alert("fail to log in!");
+      window.location.replace("datapage.html");
+  }
+    else {
+      alert("sign in!");  
+      window.location.replace("datapage.html");
+  }
+  });
+  
+  
+}
+//test sign out
+function signOut()
+{
+  firebase.auth().signOut().then(function() //same asynchronous callbacks
+  {
+    console.log('Signed Out');
+  }, function(error) 
+  {
+    console.error('Sign Out Error', error);
+  });
+}
+//used in the HTML body's onload property
+function checkSession()
+{
+  //NOTE: set timeout only needed @ large intervals for when using debugger, otherwise keep small (@ least 500 ms)
+  setTimeout(function(){ alert("Hello " + firebase.auth().currentUser.email); }, 500);
 }
